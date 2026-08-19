c548e270cbca9113aecd1fd398e67fd0b232e19b
ccc <linlu0616@gmail.com>
2026-08-20 02:56:27 +0800

    【戳】首页隐藏#不推荐阅读

--- themes/nav/components/BlogPostListAll.js
+++ themes/nav/components/BlogPostListAll.js
@@ -36,7 +36,9 @@ const BlogPostListAll = (props) => {
   })
 
   const selectedSth = false
-  const groupedArray = filteredNavPages?.reduce((groups, item) => {
+  
+  const homeFilteredPages = filteredNavPages?.filter(item => item?.category !== '不推荐阅读')  // 首页隐藏指定 category
+  const groupedArray = homeFilteredPages?.reduce((groups, item) => {
     const categoryName = item?.category ? item?.category : '' // 将category转换为字符串
     const categoryIcon = filterLinks[categoryName]?.icon ? filterLinks[categoryName]?.icon : '' // 将pageIcon转换为字符串
     let existingGroup = null
